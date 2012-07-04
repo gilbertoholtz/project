79f0db0a2a0a6be220bfd27a90e867d69e1f79a1
Gilberto Holtz <gilbertoholtz@gmail.com>
2012-07-04 19:22:12 -0300

    ultima atualizacao

--- app/models/order_line.rb
+++ app/models/order_line.rb
@@ -1,7 +1,7 @@
 class OrderLine < ActiveRecord::Base
   attr_accessible :complemento, :integer, :integer, :item_id, :po_header_id, :qtd, :vlrUnit
 
-  belongs_to :order_header
+  belongs_to :order_header, :class_name => 'OrderHeader', :foreign_key => "po_header_id"
   belongs_to :product, :class_name => 'Product', :foreign_key => "item_id"
 
 end
